fec1c12ad7804987d9b8e64f8884aa53a7d33281
Sevhalen <sevhalen@gmail.com>
2018-05-30 11:00:24 +0200

    End of Multilinear Regression from SPP

--- Machine_Learning/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine_Learning/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -18,10 +18,13 @@ onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
+# Creating a backup array X_BackUp before removing the first column of X
+# for further observation of this code
+X_BackUp = X[:, :]
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -39,3 +42,33 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# Building the Optimal Model using Backward Eliminatin
+import statsmodels.formula.api as sm
+# Adding a columns of "1" as the first column of X to trick the library
+# which doesn't consider the constant in the regression model
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+
+# Copying thr whole X matrix in the futur optimal matrix X_opt
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+# Deleting the X2 coefficient (corresponding to the influence of New York state)
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+# Deleting the X1 coefficient (corresponding to the influence of Florida State)
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+# Deleting the X4 coefficient (corresponding to the influence of Administration Spend)
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+# Trial with deletion of the X5 coefficient (corresponding to the influence of
+# Marketing Spend) - In the previoux regression, its P-value was 0.060 and
+# R² / Adjust-R² respectively of 0.950 and 0.948
+# Result : both R² criterions are altered deleting the X5 coefficient
+# X_opt = X[:, [0, 3]]
+# regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+# regressor_OLS.summary()
